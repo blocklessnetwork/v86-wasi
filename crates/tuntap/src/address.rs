b83936c1bc1bb764245bd4afea23f9204fe84551
join.g <joinhack@gmail.com>
2022-11-04 17:54:57 +0800

    the first verison tap with out poll

--- crates/tuntap/src/address.rs
+++ crates/tuntap/src/address.rs
@@ -148,8 +148,101 @@ impl From<[u8; 6]> for EthAddr {
 impl From<&str> for EthAddr {
 
     fn from(v: &str) -> Self {
-        "192".parse::<Ip4Addr>;
-        
+        let val = Parse::new(v).read_eth_addr().unwrap();
+        EthAddr(val)
     }
     
+}
+
+struct Parse<'a>(&'a [u8]);
+
+impl<'a> Parse<'a> {
+
+    fn new(s: &'a str) -> Self {
+        Self(s.as_bytes())
+    }
+
+    fn read_char(&mut self) -> Option<char> {
+        self.0.split_first().map(|(f, tail)| {
+            self.0 = tail;
+            char::from(*f)
+        })
+    }
+
+    fn read_atomically<T, F>(&mut self, inner: F) -> Option<T>
+    where
+        F: FnOnce(&mut Parse<'_>) -> Option<T>,
+    {
+        let state = self.0;
+        let result = inner(self);
+        if result.is_none() {
+            self.0 = state;
+        }
+        result
+    }
+
+    fn read_num(&mut self) -> Option<u32> {
+        const MAX_NUM: usize = 2;
+        self.read_atomically(|p| {
+            let mut maddr = 0u32;
+            let mut num = 0;
+            while let Some(a) = p.read_atomically(|p| p.read_char()?.to_digit(16)) {
+                maddr = maddr.checked_mul(16)? + a;
+                num += 1;
+                if num > MAX_NUM {
+                    return None;
+                }
+            }
+            println!("{}", num);
+            if num == 0 {
+                return None;
+            } else {
+                return Some(maddr);
+            }
+        })
+    }
+
+    fn read_given_char(&mut self, target: char) -> Option<()> {
+        self.read_atomically(|p| {
+            p.read_char().and_then(|c| if c == target { Some(()) } else { None })
+        })
+    }
+
+    fn read_separator<T, F>(&mut self, sep: char, index: usize, inner: F) -> Option<T>
+    where
+        F: FnOnce(&mut Parse<'_>) -> Option<T>,
+    {
+        self.read_atomically(move |p| {
+            if index > 0 {
+                p.read_given_char(sep)?;
+            }
+            inner(p)
+        })
+    }
+
+    fn read_eth_addr(&mut self) -> Option<[u8; 6]> {
+        self.read_atomically(|p| {
+            let mut groups = [0; 6];
+
+            for (i, slot) in groups.iter_mut().enumerate() {
+                *slot = p.read_separator(':', i, |p| {
+                    p.read_num().map(|i| i as u8)
+                })?;
+            }
+            Some(groups)
+        })
+    }
+}
+
+mod test {
+    use super::Parse;
+
+    #[test]
+    fn test() {
+        let val  = Parse::new("01:02:03:04:5:ff").read_eth_addr();
+        assert_eq!(val, Some([1,2,3,4,5,0xff]));
+
+        let val  = Parse::new("f:02:03:04:5:fe").read_eth_addr();
+        assert_eq!(val, Some([0xf,2,3,4,5,0xfe]));
+    }
 }
