65f8456714552efbcf72d62503735db299e27163
Join.G <joinhack@gmail.com>
2024-04-14 09:42:43 +0800

    refactor the macro of read/write

--- crates/wasi/src/lib.rs
+++ crates/wasi/src/lib.rs
@@ -65,12 +65,13 @@ pub use log::set_log_file_name;
 pub use log::set_log_mask;
 
 macro_rules! copy_impl {
-    ($name: ident, $type: ty, $l: literal) => {
+    ($name: ident, $type: ty) => {
         pub fn $name(src: &[u8], dst: &mut [$type]) {
-            let mut bs = [0u8; $l];
+            const SZ: usize = std::mem::size_of::<$type>();
+            let mut bs = [0u8; SZ];
             for i in 0..dst.len() {
-                let start = i * $l;
-                let end = start + $l;
+                let start = i * SZ;
+                let end = start + SZ;
                 bs.copy_from_slice(&src[start..end]);
                 let t: $type = <$type>::from_le_bytes(bs);
                 dst[i] = t;
@@ -80,36 +81,38 @@ macro_rules! copy_impl {
 }
 
 macro_rules! read_impl {
-    ($name: ident, $type: ty, $l: literal) => {
+    ($name: ident, $type: ty) => {
         pub fn $name(src: &[u8], idx: usize) -> $type {
-            let mut bs = [0u8; $l];
-            bs.copy_from_slice(&src[idx * $l..(idx * $l + $l)]);
+            const SZ: usize = std::mem::size_of::<$type>();
+            let mut bs = [0u8; SZ];
+            bs.copy_from_slice(&src[idx * SZ..(idx * SZ + SZ)]);
             <$type>::from_le_bytes(bs)
         }
     };
 }
 
 macro_rules! write_impl {
-    ($name: ident, $type: ty, $l: literal) => {
+    ($name: ident, $type: ty) => {
         pub fn $name(src: &mut [u8], idx: usize, v: $type) {
+            const SZ: usize = std::mem::size_of::<$type>();
             let bs = v.to_le_bytes();
-            let dst = &mut src[idx * $l..(idx * $l + $l)];
+            let dst = &mut src[idx * SZ..(idx * SZ + SZ)];
             dst.copy_from_slice(&bs);
         }
     };
 }
 
 pub(crate) mod utils {
-    copy_impl!(copy_to_i32s, i32, 4);
+    copy_impl!(copy_to_i32s, i32);
 
-    read_impl!(read_i32, i32, 4);
-    read_impl!(read_u32, u32, 4);
-    read_impl!(read_u16, u16, 2);
-    read_impl!(read_i16, i16, 2);
-    write_impl!(write_i32, i32, 4);
-    write_impl!(write_u32, u32, 4);
-    write_impl!(write_u16, u16, 2);
-    write_impl!(write_i16, i16, 2);
+    read_impl!(read_i32, i32);
+    read_impl!(read_u32, u32);
+    read_impl!(read_u16, u16);
+    read_impl!(read_i16, i16);
+    write_impl!(write_i32, i32);
+    write_impl!(write_u32, u32);
+    write_impl!(write_u16, u16);
+    write_impl!(write_i16, i16);
 }
 
 trait ContextTrait {
