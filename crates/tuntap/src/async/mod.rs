0e4c97ed918dc5898181cae26261a6428ec7b759
Join.G <joinhack@gmail.com>
2024-05-12 14:59:20 +0800

    add executor.

--- crates/tuntap/src/async/mod.rs
+++ crates/tuntap/src/async/mod.rs
@@ -1,15 +1,3 @@
 mod reactor;
-
-#[cfg(test)]
-mod test {
-    use futures::executor::LocalPool;
-
-    #[test]
-    fn test_future() {
-        let mut local = LocalPool::new();
-        let out = local.run_until(async {
-            std::thread::current().id()
-        });
-        assert_eq!(std::thread::current().id(), out);
-    }
-}
+mod executor;
+mod async_dev;
--- crates/tuntap/src/async/reactor.rs
+++ crates/tuntap/src/async/reactor.rs
@@ -1,7 +1,7 @@
 use std::{
     collections::HashMap, 
     io, 
-    sync::Once, 
+    sync::{Arc, Mutex, OnceLock}, 
     task::{Poll, Waker}, 
     time::Duration
 };
@@ -10,15 +10,11 @@ use crate::{
     Events, Poller, Token
 };
 
-pub fn global_reactor() -> &'static mut Reactor {
-    static ONCE: Once = Once::new();
-    static mut GLOBAL_REACTOR: Option<Reactor> = None;
-    unsafe {
-        ONCE.call_once(|| {
-            GLOBAL_REACTOR = Some(Reactor::new(Duration::from_millis(2)));
-        });
-        GLOBAL_REACTOR.as_mut().unwrap()
-    }
+pub fn global_reactor() -> Arc<Mutex<Reactor>> {
+    static GLOBAL_REACTOR: OnceLock<Arc<Mutex<Reactor>>> = OnceLock::new();
+    GLOBAL_REACTOR.get_or_init(|| {
+        Arc::new(Mutex::new(Reactor::new(Duration::from_millis(2))))
+    }).clone()
 }
 
 pub struct Reactor {
@@ -28,7 +24,7 @@ pub struct Reactor {
 }
 
 impl Reactor {
-    fn new(time_interval: Duration) -> Self {
+    pub fn new(time_interval: Duration) -> Self {
         let poller = Poller::new();
         let wakers = Default::default();
         Self {
@@ -40,7 +36,7 @@ impl Reactor {
 
     /// main loop for reactor
     /// this loop is for walking up waker.
-    fn run_once(&mut self) {
+    pub fn wait(&mut self) {
         let mut events = Events::with_capacity(1024);
         self.poller.poll(&mut events, Some(self.time_interval)).unwrap();
         for event in &events {
@@ -55,16 +51,12 @@ impl Reactor {
             .map(|waker| waker.wake());
     }
 
-    fn add_waker(&mut self, token: Token, waker: Waker) {
-        self.wakers
-            .insert(token, waker);
-
+    fn add_waker(token: Token, waker: Waker) {
+        global_reactor().lock().unwrap().wakers.insert(token, waker);
     }
 
-    fn remove_waker(&mut self, token: Token) {
-        self.wakers
-            .remove(&token);
-
+    fn remove_waker(token: Token) {
+        global_reactor().lock().unwrap().wakers.remove(&token);
     }
 }
 
@@ -77,11 +69,11 @@ pub fn try_invoke<F, T>(
 where 
     F: FnMut() -> io::Result<T>
 {
-    global_reactor().add_waker(token, waker);
+    Reactor::add_waker(token, waker);
     loop {
         match f() {
             Ok(r) => {
-                global_reactor().remove_waker(token);
+                Reactor::remove_waker(token);
                 return Poll::Ready(Ok(r));
             },
             Err(err) if err.kind() == io::ErrorKind::WouldBlock => {
@@ -91,7 +83,7 @@ where
                 continue;
             }
             Err(err) => {
-                global_reactor().remove_waker(token);
+                Reactor::remove_waker(token);
                 return Poll::Ready(Err(err));
             }
         }
